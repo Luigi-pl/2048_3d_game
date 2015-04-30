55ff95fb7401286a6b469762399b7cb6d25b4de2
Luigi <Lukaszurawski@gmail.com>
2015-04-30 15:30:56 +0200

    First version of game model was added

---
+++ 2048_3d_game/Field.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Shapes.Rectangle;
+using Windows.UI.Xaml.Controls.Viewbox;
+using Windows.UI.Xaml.Controls.TextBlock;
+using Windows.UI.Xaml.Controls.Grid;
+using UI = Windows.UI.Xaml;
+namespace _2048_3d_game
+{
+    class Field
+    {
+        private UI.Shapes.Rectangle background;
+        private UI.Controls.Viewbox contentBox;
+        private UI.Controls.TextBlock content;
+
+        Field()
+        {
+            background = new UI.Shapes.Rectangle();
+            background.Margin = new UI.Thickness(0);
+
+            contentBox = new UI.Controls.Viewbox();
+            content = new UI.Controls.TextBlock();
+        }
+    }
+}
